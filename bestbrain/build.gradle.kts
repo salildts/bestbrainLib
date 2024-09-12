c5f346566c7ace3249f69a944980672cc3974308
dts1409 <soubhik.de@dreamztech.com>
2024-09-12 14:06:47 +0530

    update

--- bestbrain/build.gradle.kts
+++ bestbrain/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.library") version "8.0.2" // Stable AGP version
+    id("org.jetbrains.kotlin.android") version "1.8.0"
     `maven-publish`
 }
 
