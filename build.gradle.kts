ec004920b30d78973e7730332acb252df3a49cfe
dts1409 <soubhik.de@dreamztech.com>
2024-09-12 12:12:12 +0530

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    `maven-publish`
 }
 
 android {
@@ -36,7 +37,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -49,8 +49,60 @@ dependencies {
     api("com.squareup.retrofit2:converter-gson:2.5.0")
     api("com.squareup.okhttp3:logging-interceptor:4.11.0")
     api("io.socket:socket.io-client:2.1.0") {
-        exclude (group= "org.json", module= "json")
+        exclude(group = "org.json", module = "json")
     }
     api("com.google.android.flexbox:flexbox:3.0.0")
     api("com.github.ybq:Android-SpinKit:1.4.0")
+    implementation(kotlin("stdlib"))
+}
+
+tasks.withType<Jar> {
+    from(sourceSets["main"].output)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+            }
+
+            groupId = "com.github.salildts"
+            artifactId = "bestbrainLib"
+            version = "1.0.0"
+
+            pom {
+                name.set("BestBrain Library")
+                description.set("A library for managing BestBrain features.")
+                url.set("https://github.com/salildts/bestbrainLib")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("salildts")
+                        name.set("Salil")
+                        email.set("salildts@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/salildts/bestbrainLib.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:salildts/bestbrainLib.git")
+                    url.set("https://github.com/salildts/bestbrainLib")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
 }
